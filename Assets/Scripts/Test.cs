cb7098d541f8cf94aca37fc9fed814f43e136da2
LiRui <18335929360@163.com>
2025-06-29 15:46:45 +0800

    [feat]测试加载配置代码

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,20 @@
+using Engine;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        ConfigSystem.Initialize();
+        Debug.Log("ConfigSystem loaded successfully.");
+        var config = ConfigSystem.Tables.TbTest3.Get(1);
+        Debug.Log("");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
